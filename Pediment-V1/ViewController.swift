45c3a7ae7c87312ddedc9e47bae07aa1bb907213
wafeeq-ridhuan <kyman101@hotmail.com>
2018-01-31 14:33:18 -0800

    Initial Commit

---
+++ Pediment-V1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pediment-V1
+//
+//  Created by Wafeeq on 1/31/18.
+//  Copyright © 2018 The Only Wafeeq. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
